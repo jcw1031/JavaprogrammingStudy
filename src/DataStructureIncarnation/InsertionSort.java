eaf4d66296cd4af36bafd1c62a6456d689360d26
woopaca <jcw001031@gmail.com>
2022-06-13 21:59:05 +0900

    modify 'InsertionSort'

--- src/DataStructureIncarnation/InsertionSort.java
+++ src/DataStructureIncarnation/InsertionSort.java
@@ -1,14 +1,14 @@
 package DataStructureIncarnation;
 
 public class InsertionSort {
-    final int count=10;
+    final int SIZE=10;
     int[] array_data = {14, 5, 43, 27, 18, 31, 37, 88, 6, 35};
 
     public void insertSort(){
         int i;
         int tmp;
         int interCnt;
-        for(i=1;i<count;i++){
+        for(i=1;i<SIZE;i++){
             tmp=array_data[i];
             interCnt=i-1;
             while(interCnt>=0 && tmp<array_data[interCnt]){
@@ -21,28 +21,17 @@ public class InsertionSort {
         }
     }
 
-    public void write_head_1(){
-        System.out.println("\n           ---------------------------");
-        System.out.println("           Source data  printing .....");
-        System.out.println("           ---------------------------");
-    }
-    public void write_head_2(){
-        System.out.println("\n           ---------------------------");
-        System.out.println("           Insertion sort result .....");
-        System.out.println("           ---------------------------\n");
-    }
-
     public void write_1(){
         int i;
         System.out.print("         ");
-        for(i=0;i<count;i++){
+        for(i=0;i<SIZE;i++){
             System.out.print("\t"+array_data[i]);
         }
         System.out.println();
     }
     public void write_2(){
         int i;
-        for(i=0;i<count;i++){
+        for(i=0;i<SIZE;i++){
             System.out.print("\t"+array_data[i]);
         }
         System.out.println();
@@ -50,10 +39,8 @@ public class InsertionSort {
 
     public static void main(String[] args) {
         InsertionSort is = new InsertionSort();
-        is.write_head_1();
         is.write_1();
         is.insertSort();
-        is.write_head_2();
         is.write_2();
     }
 }

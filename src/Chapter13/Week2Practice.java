273deeb83dd079acd12a292ec908b3821cddd22a
woopaca <jcw001031@gmail.com>
2022-09-14 00:22:59 +0900

    file output/input practice

---
+++ src/Chapter13/Week2Practice.java
@@ -0,0 +1,46 @@
+package Chapter13;
+
+import java.io.*;
+import java.util.*;
+
+public class Week2Practice {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner s = new Scanner(new FileInputStream("/Users/jcw/Coding/words.txt"));
+
+        Vector<String> wordVector = new Vector<>();
+
+        while(s.hasNext()){
+            wordVector.add(s.nextLine());
+        }
+
+        while(true){
+            boolean flag;
+            String search;
+            System.out.print("단어 : ");
+            search = br.readLine();
+            if(Objects.equals(search, "그만")) {
+                System.out.println("종료합니다.");
+                break;
+            }
+            for(int i=0;i<wordVector.size();i++){
+                flag = true;
+                String tmp = wordVector.get(i);
+                for(int j = 0;j<search.length();j++){
+                    if(tmp.length() < search.length()){
+                        flag = false;
+                        break;
+                    }
+                    if(tmp.charAt(j) != search.charAt(j)){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    System.out.println(tmp);
+                }
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Random random = new Random();
-        int start = 0, end = 99;
-        int[] bound = new int[100];
-        int answer = random.nextInt(100);
+        StringBuilder sb = new StringBuilder();
+        FileOutputStream fos = new FileOutputStream("data.txt");
+        DataOutputStream dos = new DataOutputStream(fos);
 
-        System.out.println("수를 결정했음. 맞춰보셈");
+        dos.writeChar('a');
+        dos.writeBoolean(true);
+        dos.writeLong(Long.MAX_VALUE);
 
-        int count = 1;
-        while(true){
-            System.out.println(start+"~"+end);
-            try {
-                System.out.print(count+">> ");
-                int input = Integer.parseInt(br.readLine());
-                bound[input-start] = 0;
-                if(input == answer){
-                    System.out.println("정답");
-                    break;
-                }
-                else if(input > answer){
-                    System.out.println("더 낮게");
-                    end = input;
-                    bound = new int[end+1];
-                }
-                else{
-                    System.out.println("더 높게");
-                    start = input;
-                    bound = new int[end-start+1];
-                }
-                count++;
-            }
-            catch(NumberFormatException e){
-                System.out.println("정수만 입력하셈");
-                continue;
-            }
-            catch(IndexOutOfBoundsException e){
-                System.out.println("범위를 벗어남");
-                continue;
-            }
-        }
+        dos.close();
+        fos.close();
     }
 }
-

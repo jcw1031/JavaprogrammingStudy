e6b9e2d32ac613a15d9e67bed8794f1297ee87e3
woopaca <jcw001031@gmail.com>
2022-10-18 00:07:35 +0900

    자바 과제1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,21 @@ class ThreadExample implements Runnable {
     @Override
     public void run() {
         System.out.println("thread start.");
-        for(int i=0;i<100;i++) {
-            System.out.println(i);
+        for(int i=0;i<100;i++){
+            System.out.print("1");
         }
         System.out.println("thread end.");
+    }
+}
+
+class ThreadExample2 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("thread2 start.");
+        for(int i=0;i<100;i++){
+            System.out.print("2");
+        }
+        System.out.println("thread2 end.");
     }
 }
 
@@ -14,9 +25,18 @@ public class Main {
         ThreadExample r = new ThreadExample();
         Thread thread = new Thread(r);
 
+        ThreadExample2 rr = new ThreadExample2();
+        Thread thread2 = new Thread(rr);
+
+        thread.setPriority(1);
+        thread2.setPriority(10);
+
+        thread2.start();
         thread.start();
 //        thread.join();
 
         System.out.println("main end.");
     }
+
+
 }
---
+++ src/subject/Subject1.java
@@ -0,0 +1,25 @@
+package subject;
+
+import javax.swing.*;
+import java.awt.*;
+
+class MovingThread extends Thread{
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class BubbleGame extends JFrame {
+    private Container ct;
+    public BubbleGame(){
+        ct = getContentPane();
+        ct.setLayout(null);
+
+
+    }
+}
+
+public class Subject1 {
+}
